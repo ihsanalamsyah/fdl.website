a86018946c40468411b97fb8f80a9a9871386876
Ihsan <ihsanalamsyah596@gmail.com>
2024-02-12 17:44:13 +0700

    fix image

--- src/ListHarga.js
+++ src/ListHarga.js
@@ -12,7 +12,7 @@ function ListHarga() {
     return (
         <>
         
-        <div className='row bg'>
+        <div className='row bg min-vh-100'>
             <div className='col-md-1'>
 
             </div>
@@ -22,49 +22,49 @@ function ListHarga() {
             </Button>
             <Carousel className='mt-1'>
                 <Carousel.Item>
-                    <img src={image1} class="w-75" alt=""/>
+                    <img src={image1} class="w-75 h-100" alt="1"/>
                     {/* <Carousel.Caption>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src={image2} class="w-75" alt=""/>
+                    <img src={image2} class="w-75" alt="2"/>
                     {/* <Carousel.Caption>
                         <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src={image3} class="w-75" alt=""/>
+                    <img src={image3} class="w-75" alt="3"/>
                     {/* <Carousel.Caption>
                         <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src={image4} class="w-75" alt=""/>
+                    <img src={image4} class="w-75" alt="4"/>
                     {/* <Carousel.Caption>
                         <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src={image5} class="w-75" alt=""/>
+                    <img src={image5} class="w-75" alt="5"/>
                     {/* <Carousel.Caption>
                         <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src={image6} class="w-75" alt=""/>
+                    <img src={image6} class="w-75" alt="6"/>
                     {/* <Carousel.Caption>
                         <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src={image7} class="w-75" alt=""/>
+                    <img src={image7} class="w-75" alt="7"/>
                     {/* <Carousel.Caption>
                         <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
