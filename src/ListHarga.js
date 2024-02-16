59d7b48537851e14d622fb526c7d2d8552b5a921
Ihsan <ihsanalamsyah596@gmail.com>
2024-02-16 14:36:42 +0700

    fix mobile css

--- src/ListHarga.js
+++ src/ListHarga.js
@@ -5,6 +5,8 @@ import image3 from './images/3.png';
 import image4 from './images/4.png';
 import image5 from './images/5.png';
 import image6 from './images/6.png';
+import walogo from './images/whatsapp.png';
+
 import './ListHarga.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -46,15 +48,29 @@ function ListHarga() {
                     modules={[EffectCoverflow, Pagination, Navigation]}
                     className="mySwiper w-75"
                 >
-                    <SwiperSlide><img src={image1} className="img-listharga" alt="1"/></SwiperSlide>
-                    <SwiperSlide><img src={image2} className="img-listharga" alt="2"/></SwiperSlide>
-                    <SwiperSlide><img src={image3} className="img-listharga" alt="3"/></SwiperSlide>
-                    <SwiperSlide><img src={image4} className="img-listharga" alt="4"/></SwiperSlide>
-                    <SwiperSlide><img src={image5} className="img-listharga" alt="5"/></SwiperSlide>
-                    <SwiperSlide><img src={image6} className="img-listharga" alt="6"/></SwiperSlide>
+                    <div className='col-12 col-lg-12 mb-2'>
+                        <SwiperSlide><img src={image1} className="img-listharga" alt="1"/></SwiperSlide>
+                        <SwiperSlide><img src={image2} className="img-listharga" alt="2"/></SwiperSlide>
+                        <SwiperSlide><img src={image3} className="img-listharga" alt="3"/></SwiperSlide>
+                        <SwiperSlide><img src={image4} className="img-listharga" alt="4"/></SwiperSlide>
+                        <SwiperSlide><img src={image5} className="img-listharga" alt="5"/></SwiperSlide>
+                        <SwiperSlide><img src={image6} className="img-listharga" alt="6"/></SwiperSlide>
+                    </div>
+                   
                 </Swiper>
            </div>
-              
+            {/* <div className='footer'>               
+                <a target="_blank" href="https://wa.me/6282114532993?text=Saya%20ingin%20operasi%20gigi%20bagaimana%20?%20">
+                    <img src={walogo} className="wa-logo" alt="wa" />
+                </a>
+                <div class="row no-gutters">
+			        <div class="col-md-3">
+			            <div class="chat-bubble chat-bubble-left">
+				        Hello dude!
+			            </div>
+			        </div>
+		        </div>
+            </div>  */}
                    
         </div>
        
